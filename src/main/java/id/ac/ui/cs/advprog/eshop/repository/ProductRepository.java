d644e0a07bacbb17bb0017a67599f6f334fb5ae9
RobertBenyamin <robertbenyamino@gmail.com>
2024-02-05 16:18:22 +0700

    feat: create delete product logic

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -19,4 +19,8 @@ public class ProductRepository {
     public Iterator<Product> findAll() {
         return productData.iterator();
     }
+
+    public void delete(Product product) {
+        productData.remove(product);
+    }
 }
