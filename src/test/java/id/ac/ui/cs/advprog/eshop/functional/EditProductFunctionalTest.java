6cfc5b69d358aae0ad9dcdec59c7b1454dab5272
RobertBenyamin <robertbenyamino@gmail.com>
2024-02-07 01:22:55 +0700

    feat: add create, edit, and delete product functional tests

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/EditProductFunctionalTest.java
@@ -0,0 +1,69 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+public class EditProductFunctionalTest {
+    /**
+     * The port number assigned to the running application during test execution. * Set automatically during each test run by Spring Framework's test context. */
+    @LocalServerPort
+    private int serverPort;
+
+    /**
+     * The base URL for testing. Default to {@code http://localhost}.
+     */
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void editProduct_isCorrect (ChromeDriver driver) throws Exception {
+        // Exercise
+        driver.get(baseUrl);
+
+        driver.findElement(By.id("product")).click();
+        driver.findElement(By.id("create")).click();
+
+        driver.findElement(By.id("nameInput")).clear();
+        driver.findElement(By.id("nameInput")).sendKeys("Sampo Cap Bambang");
+        driver.findElement(By.id("quantityInput")).clear();
+        driver.findElement(By.id("quantityInput")).sendKeys("100");
+
+        driver.findElement(By.id("submit")).click();
+
+        driver.findElement(By.id("edit_1")).click();
+
+        driver.findElement(By.id("nameInput")).clear();
+        driver.findElement(By.id("nameInput")).sendKeys("Sampo Cap Bango");
+        driver.findElement(By.id("quantityInput")).clear();
+        driver.findElement(By.id("quantityInput")).sendKeys("50");
+
+        driver.findElement(By.id("submit")).click();
+
+        // Verify
+        WebElement product = driver.findElement(By.id("name_1"));
+        assertEquals(product.getText(), "Sampo Cap Bango");
+        
+        product = driver.findElement(By.id("quantity_1"));
+        assertEquals(product.getText(), "50");
+    }
+}
