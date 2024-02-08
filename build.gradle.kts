8b49d9f24876126b8cacdb7ccb8b68cfe3f92884
RobertBenyamin <robertbenyamino@gmail.com>
2024-02-08 23:20:07 +0700

    chore: add Jacoco plugin and configure test tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -60,6 +61,18 @@ tasks.register<Test>("functionalTest"){
     filter{
         includeTestsMatching("*FunctionalTest")
     }
+}
+
+tasks.test {
+	filter {
+		excludeTestsMatching("*FunctionalTest")
+	}
+
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
 }
 
 tasks.withType<Test>().configureEach {
