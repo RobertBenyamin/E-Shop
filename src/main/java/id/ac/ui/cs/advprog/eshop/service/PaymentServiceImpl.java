7af12d099f8b116ccc339ffede77aa94355d19ac
RobertBenyamin <robertbenyamino@gmail.com>
2024-03-05 15:50:08 +0700

    [GREEN] Implement PaymentService class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -17,18 +17,27 @@ public class PaymentServiceImpl implements PaymentService {
     private PaymentRepository paymentRepository;
 
     public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
-        return null;
+        Payment payment = new Payment("", method, paymentData, order);
+        payment = paymentRepository.save(payment);
+        return payment;
     }
 
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        Payment savedPayment = paymentRepository.findById(payment.getId());
+        if (savedPayment != null) {
+            savedPayment.setStatus(status);
+            paymentRepository.save(savedPayment);
+            return savedPayment;
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.getAllPayment();
     }
 }
