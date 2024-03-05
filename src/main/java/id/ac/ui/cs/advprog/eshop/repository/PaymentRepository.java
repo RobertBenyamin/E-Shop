ccac8abbc0d55904397e967c168c2f4eaecc82f6
RobertBenyamin <robertbenyamino@gmail.com>
2024-03-05 10:49:07 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -9,18 +9,49 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+        paymentData.add(payment);
         return payment;
     }
-    public Payment findById(String id){
+
+    public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
         return null;
     }
-    public List<Payment> findAllByVoucherCode(){
-        return null;
+
+    public List<Payment> findAllByVoucherCode() {
+        List<Payment> result = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getMethod().equals("VOUCHER_CODE")) {
+                result.add(savedPayment);
+            }
+        }
+        return result;
     }
-    public List<Payment> findAllByCashOnDelivery(){
-        return null;
+
+    public List<Payment> findAllByCashOnDelivery() {
+        List<Payment> result = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getMethod().equals("CASH_ON_DELIVERY")) {
+                result.add(savedPayment);
+            }
+        }
+        return result;
     }
-    public List<Payment> getAllPayment(){
-        return null;
+
+    public List<Payment> getAllPayment() {
+        return paymentData;
     }
 }
